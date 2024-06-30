3517eae40556b32aca2afef46fa5a68fcb300a79
Khang Tran <creamybanana@Khang.local>
2024-06-29 17:34:31 -0700

    added NavLinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"
 
 
 
@@ -5,9 +6,20 @@ const Navbar = () => {
     return (
         //  having the header section is good for SEO cause it helps search engines to understand the content of the page
         <header className="header">
-            <Navlink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
 
-            </Navlink>
+            <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+                <p className="blue-gradient_text">Khang</p>
+            </NavLink>
+
+            <nav className="flex text-lg gap-7 font-medium">
+                <NavLink to="/about" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                    About
+                </NavLink>
+
+                <NavLink to="/projects" className={({ isActive }) => isActive ? 'text-blue-500' : 'text-black'}>
+                    Projects
+                </NavLink>
+            </nav>
 
         </header>
     )
