a698e57cb27cac3419fbaaaadef699766b50c391
Khang Tran <creamybanana@Khang.local>
2024-06-29 17:03:08 -0700

    added the NAV section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+
+
+const Navbar = () => {
+    return (
+        //  having the header section is good for SEO cause it helps search engines to understand the content of the page
+        <header className="header">
+            <Navlink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
+
+            </Navlink>
+
+        </header>
+    )
+}
+
+export default Navbar
