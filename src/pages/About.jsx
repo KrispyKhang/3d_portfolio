b9830857886f992a85a5c22f982aaf067334e3ea
Khang Tran <creamybanana@Khang.local>
2024-07-05 15:53:05 -0700

    implemented about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,26 @@
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className="max-container">
+            <h1 className="head-text">
+                Hello I'm <span className="blue-gradient_text font-semibold drop-shadow"> Khang Tran</span>
+            </h1>
+
+            <div className="mt-5 flex flex-col gap-3">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus amet aliquam animi repellat eligendi, commodi nemo, voluptatum dolorum suscipit earum,
+                    labore laboriosam optio nihil. Impedit repudiandae qui soluta iusto alias.</p>
+            </div>
+
+            <div className=" py-10 flex flex-col">
+                <h3 className="subhead-text"> My Skills</h3>
+            </div>
+
+            <div className="mt-16 flex flex-wrap gap-12">
+
+
+
+            </div>
+        </section>
     )
 }
 
