b0bcd3074125f5102ddc01492db07195dbfe80c0
Khang Tran <creamybanana@Khang.local>
2024-07-07 17:21:43 -0700

    added portfolio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,9 @@
-import { skills, languages } from '../constants'
+import { skills, languages, experiences } from '../constants'
+
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+
 
 const About = () => {
     return (
@@ -33,7 +38,7 @@ const About = () => {
                 ))}
             </div>
 
-            <div className='py-5' />
+            <div className='py-10' />
 
             <div className=" py-5 flex flex-col">
                 <h3 className="subhead-text"> Technologies & Frameworks</h3>
@@ -55,21 +60,72 @@ const About = () => {
             </div>
 
 
-            <div className='py-20' />
+            <div className='py-5' />
 
 
             <div className='py-5'>
                 <h3 className='subhead-text'>
                     Work Experience
-
                 </h3>
+                <div className="mt-5 flex flex-col gap-3">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus amet aliquam animi repellat eligendi, commodi nemo, voluptatum dolorum suscipit earum,
+                        labore laboriosam optio nihil. Impedit repudiandae qui soluta iusto alias.</p>
+                </div>
+
+                <div className='mt-12 flex'>
+                    <VerticalTimeline>
+                        {experiences.map((experience) => (
+                            <VerticalTimelineElement
+                                key={experience.company_name}
+                                date={experience.date}
+                                icon={
+                                    <div className='flex justify-center items-center w-full h-full'>
+                                        <img
+                                            src={experience.icon}
+                                            alt={experience.company_name}
+                                            className='w-[60%] h-[60%] object-contain'
+                                        />
+                                    </div>}
+                                iconStyle={{ background: experience.iconBg }}
+                                contentStyle={{
+                                    borderBottom: '8px',
+                                    borderStyle: 'solid',
+                                    borderBottomColor: experience.iconBg,
+                                    boxShadow: 'none,'
+                                }}
+                            >
+                                <div>
+                                    <h3 className='text-black text-xl font-poppins font-semibold'>
+                                        {experience.title}
+                                    </h3>
+                                    <p className='text-black-500 font-medium font-base' style={{ margin: 0 }}>
+                                        {experience.company_name}
+                                    </p>
+                                </div>
+
+                                <ul className='my-5 list-disc ml-5 space-y-2'>
+                                    {experience.points.map((point, index) => (
+                                        <li
+                                            key={`${experience.points}-${index}`}
+                                            className='text-black-500/50 font-normal pl-1 text-sm'>{point}</li>
+                                    ))}
+                                </ul>
+
+
+
+                            </VerticalTimelineElement>
+
+
+                        ))};
+                    </VerticalTimeline>
+
+                </div>
 
             </div>
 
-
-
-
-        </section>
+            {/*  creates a nice line to divide to the next session */}
+            <hr className='border-slate-200' />
+        </section >
     )
 }
 
